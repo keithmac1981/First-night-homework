0e3642483986ed5e13262bebb4f495d289f45f22
olliebingo <olliebingo@hotmail.com>
2020-04-01 00:10:50 +0100

    second night homework

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,37 @@
+def return_10()
+    return 5 + 5
+end
+
+def add(first_number, second_number)
+    return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+    return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+    return first_number * second_number
+end
+
+def divide(first_number, second_number)
+    return first_number / second_number
+end
+
+def length_of_string(a_string)
+    return a_string.length
+end
+
+def join_string(string_one, string_two)
+    string_one = 'Mary had a little lamb, '
+    string_two = 'its fleece was white as snow'
+    return string_one + string_two
+end
+
+def add_string_as_number(string_one, string_two)
+    string_one = 1
+    string_two = 2
+    return string_one.to_i + string_two.to_i
+end
+
+
